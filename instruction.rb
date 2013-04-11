2d92b66cc4df7e3bef42a7626bf8e9c5aafd8f8b
Matt Swain <mds1482@rit.edu>
2013-04-11 14:52:20 -0400

    Removed InstructionHandler and InstructionList

--- instruction.rb
+++ instruction.rb
@@ -6,31 +6,3 @@ class Instruction
         @data = data
     end
 end
-
-class InstructionHandler
-    attr :type
-
-    def type()
-        return @type
-    end
-
-    def run(data)
-        raise NoMethodError
-    end
-end
-
-class InstructionList
-    attr :instr_list
-
-    def initialize()
-        @instr_list = []
-    end
-
-    def add(instruction)
-        unless instruction.is_a? Instruction
-            raise "Internal: Invalid argument '#{instruction}'"
-        end
-
-        instr_list<< instruction
-    end
-end
